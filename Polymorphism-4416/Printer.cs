e7a62e98eb56731d6cde78462c2a74ab63ebd6d9
rahmanhakim7 <hakimrahman@students.amikom.ac.id>
2022-06-17 18:18:24 +0700

    Add project files.

---
+++ Polymorphism-4416/Printer.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorphism_4416
+{
+    internal class PrinterWindows 
+    {
+        public virtual void Show()
+        {
+            Console.WriteLine("Printer Display Dimension");
+        }
+        public virtual void Print()
+        {
+            Console.WriteLine("Printer Printing ");
+        }
+    }
+
+    internal class Epson : PrinterWindows
+    {
+        public override void Show ()
+        {
+            Console.WriteLine("Epson Display Dimension : 10 * 11");
+        }
+
+        public override void Print()
+        {
+            Console.WriteLine("Epson Printer Printing ");
+        }
+    }
+
+    internal class Canon : PrinterWindows
+    {
+        public override void Show()
+        {
+            Console.WriteLine("Canon Display Dimension : 9.5 * 12");
+        }
+
+        public override void Print()
+        {
+            Console.WriteLine("Canon Printer Printing");
+        }
+    }
+
+    internal class LaserJet : PrinterWindows
+    {
+        public override void Show()
+        {
+            Console.WriteLine("LaserJet Display Dimension : 12 * 12");
+        }
+
+        public override void Print()
+        {
+            Console.WriteLine("LaserJet Printer Printing");
+        }
+    }
+}
